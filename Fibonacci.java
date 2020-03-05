1bc2ab821862709176d5371b76d1c14d85ac7cd4
QQQQinghuan <1102070436@qq.com>
2020-03-05 16:31:13 +0800

    敏捷web第二周作业

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,16 @@
-class Fibonacci {
+package webTest;
+import java.math.BigDecimal;
+
+public class Fibonacci {
     public static void main(String[] args) {
         for (int i = 1; i < 200; i++) {
             System.out.println(of(i));
         }
     }
 
-    static int of(int num) {
-        if (num == 1 || num == 2) return num;
-        else return of(num - 1) + of(num - 2);
+    static BigDecimal of(int num) {
+        if (num == 1 || num == 2) return BigDecimal.valueOf(1);
+        else return of(num - 1).add(of(num - 2));
 
     }
 }
